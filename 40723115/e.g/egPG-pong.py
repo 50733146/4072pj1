9d05c0f40e28eee7f151f444bc495b5ea1af032a
s40723115 <40723115@gm.nfu.edu.tw>
2021-03-05 08:33:19 +0800

    add annotation in egPG-pong

--- 40723115/e.g/egPG-pong.py
+++ 40723115/e.g/egPG-pong.py
@@ -11,19 +11,22 @@ gamma = 0.99 # discount factor for reward
 decay_rate = 0.99 # decay factor for RMSProp leaky sum of grad^2
 resume = False # 從先前的檢查點回復?
 render = False
+
+
 # 模組初始化
 D = 80 * 80  # 輸入維度:80x80網格
 if resume:
     model = pickle.load(open('save.p', 'rb'))
 else:
-    model = {}
-    #dn表格每個維度
+    model = {} #空字典    #dn表格每個維度
     #返回值為指定維度的array
 model['W1'] = np.random.randn(H, D) / np.sqrt(D)  # "Xavier" initialization
 model['W2'] = np.random.randn(H) / np.sqrt(H)#randn函式返回一個或一組樣本，具有標準正態分佈。
 
+
 grad_buffer = {k: np.zeros_like(v) for k, v in model.items()}  # update buffers that add up gradients over a batch
 rmsprop_cache = {k: np.zeros_like(v) for k, v in model.items()}  # rmsprop memory
+
 #壓縮功能到0和1之間
 def sigmoid(x):
   return 1.0 / (1.0 + np.exp(-x)) # sigmoid "squashing" function to interval [0,1]
@@ -36,14 +39,15 @@ def prepro(I):
   I[I == 144] = 0 # erase background (background type 1)
   I[I == 109] = 0 # erase background (background type 2)
   I[I != 0] = 1 # everything else (paddles, ball) just set to 1
-  return I.astype(np.float).ravel()
+  return I.astype(np.float).ravel() #對資料型別進行轉換 #ravel():将多维数组转换为一维数组的功能,如果没有必要，不会产生源数据的副本
 
 def discount_rewards(r):
   """ take 1D float array of rewards and compute discounted reward """
 #採取一為浮動獎勵數組並計算折扣獎勵
+  #γ 越低，agent 越重視當前所獲得的 reward，並覺得未來獲得的 reward 太遙遠，不足以在當前 state 的決策過程中佔有太大份量。
   discounted_r = np.zeros_like(r) #與r陣列型別一致的陣列
   running_add = 0
-  for t in reversed(range(0, r.size)):
+  for t in reversed(range(0, r.size)):#reversed:函式接受一個列表作為引數，並返回同一個列表的反轉版本的迭代器。
     if r[t] != 0: running_add = 0 # 重製總和, 因為這是遊戲邊界 (pong specific!)
     running_add = running_add * gamma + r[t]
     discounted_r[t] = running_add
@@ -65,24 +69,28 @@ def policy_backward(eph, epdlogp):
   dh[eph <= 0] = 0 # backpro prelu
   dW1 = np.dot(dh.T, epx)
   return {'W1':dW1, 'W2':dW2}
+
+
 env = gym.make("Pong-v0")
 observation = env.reset()
 prev_x = None # 用於計算差異幀
-xs,hs,dlogps,drs = [],[],[],[]
+xs,hs,dlogps,drs = [],[],[],[] #空列表
 running_reward = None
 reward_sum = 0
 episode_number = 0
-while True:
-  if render: env.render()
 
-  # 對觀測值進行預處理, 將差異圖像設置為input
+
+while True:
+  if render: env.render()#是每个环境文件都包含的函数
+
+
   cur_x = prepro(observation)
   x = cur_x - prev_x if prev_x is not None else np.zeros(D)
   prev_x = cur_x
 
   # forward the policy network and sample an action from the returned probability
   aprob, h = policy_forward(x)
-  action = 2 if np.random.uniform() < aprob else 3 # roll the dice!
+  action = 2 if np.random.uniform() < aprob else 3 #從一個均勻分佈[low,high)中隨機取樣，注意定義域是左閉右開，即包含low，不包含high.
 
   # record various intermediates (needed later for backprop)
   xs.append(x) # observation #用於在列表末尾添加新的對象也就是x
@@ -91,9 +99,9 @@ while True:
   dlogps.append(y - aprob) # grad that encourages the action that was taken to be taken (see http://cs231n.github.io/neural-networks-2/#losses if confused)
 
   #步入環境並獲得新的測量結果
-  observation, reward, done, info = env.step(action)
+  observation, reward, done, info = env.step(action)#選擇一個action(動作)，並前進一偵，並得到新的環境參數會回傳四個值，依序是observation,reward,done,info
   reward_sum += reward
-
+#在list末尾增加reward值
   drs.append(reward) # record reward (has to be done after we call step() to get reward for previous action)
 
   if done: # an episode finished
@@ -119,13 +127,13 @@ while True:
     for k in model: grad_buffer[k] += grad[k] # 累積 grad over batch
 
     # perform rmsprop parameter update every batch_size episodes
-    if episode_number % batch_size == 0:
+    if episode_number % batch_size == 0:#相除取餘數#==是判斷兩個變數之間的『值』是否相同
       for k,v in model.items():
         g = grad_buffer[k] # gradient
-        rmsprop_cache[k] = decay_rate * rmsprop_cache[k] + (1 - decay_rate) * g**2
+        rmsprop_cache[k] = decay_rate * rmsprop_cache[k] + (1 - decay_rate) * g**2 #**執行對操作指數（冪）的計算
         model[k] += learning_rate * g / (np.sqrt(rmsprop_cache[k]) + 1e-5)
         grad_buffer[k] = np.zeros_like(v) # 重置批次梯度buffer
-        #print(grad_buffer['W1'])
+        print(grad_buffer['W1'])
 
     # boring book-keeping
     running_reward = reward_sum if running_reward is None else running_reward * 0.99 + reward_sum * 0.01
@@ -135,5 +143,5 @@ while True:
     observation = env.reset() # reset env
     prev_x = None
 
-  if reward != 0: # Pong has either +1 or -1 reward exactly when game ends.
+  if reward != 0: # Pong has either +1 or -1 reward exactly when game ends.#檢查兩個操作數的值是否相等，如果值不相等，則條件變為真
     print ('ep %d: game finished, reward: %f' % (episode_number, reward) + ('' if reward == -1 else ' !!!!!!!!'))
