350c9e8957bbad40ca0e8259ed6988a512c8ebc5
s40723150 <40723150@gm.nfu.edu.tw>
2021-09-29 07:31:25 +0800

    add pong1.1 training data

--- 40723150/training data/V1toV2/show_rewardList.py
+++ 40723150/training data/V1toV2/show_rewardList.py
@@ -1,25 +1,25 @@
 import numpy as np
 from matplotlib.backends.backend_pdf import PdfPages
 import matplotlib.pyplot as plt
-'''file1_name = "pong2_rlist_10K.txt"
-file1_sum_name = "pong2_Srlist_10K.txt"
-file2_name = "pong2_r_rlist.txt"
-file2_sum_name = "pong2_r_Srlist.txt"
-reward1_data = np.loadtxt(file1_name)
-reward1_sum_data = np.loadtxt(file1_sum_name)
-reward2_data = np.loadtxt(file2_name)
-reward2_sum_data = np.loadtxt(file2_sum_name)
+pong1r, pong1s = ".\pong1\pong1_rlist.txt", ".\pong1\pong1_Srlist.txt"
+
+pong1_1r, pong1_1s = ".\pong1.1\pong1.1_rlist.txt", ".\pong1.1\pong1.1_Srlist.txt"
+pong1_2r, pong1_2s = ".\pong1.2\pong1.2_rlist.txt", ".\pong1.2\pong1.2_Srlist.txt"
+pong2r, pong2s = ".\pong2\pong2_rlist.txt", ".\pong2\pong2_Srlist.txt"
+
+p1rd, p1sd = np.loadtxt(pong1r), np.loadtxt(pong1s)
+p2rd, p2sd = np.loadtxt(pong2r), np.loadtxt(pong2s)
 fig, ax = plt.subplots()
 #plt.plot(reward1_data)
-ax.plot(reward1_data, "r,", label = "pong2")
-ax.plot(reward1_sum_data, "r", label = "pong2 MSE")
-ax.plot(reward2_data, "c,", label = "pong2_r")
-ax.plot(reward2_sum_data, "c", label = "pong2_r MSE")
+ax.plot(p1rd, "r,", label = "pong1")
+ax.plot(p1sd, "r", label = "pong1 MSE")
+ax.plot(p2rd, "g,", label = "pong2")
+ax.plot(p2sd, "g", label = "pong2 MSE")
+
 ax.set(xlabel='episode time', ylabel='episode reward',
-       title='Pong2 ' + 'Reward trend graph')
+       title='Pong1 ' + 'Reward trend graph')
 ax.legend()
 plt.grid()
 fig.savefig('pong2.png', dpi=100)
 #plt.savefig('reward.pdf')
-plt.show()'''
-print(np.array(list(range(0, 16)) + list(range(144,170))))
+plt.show()
