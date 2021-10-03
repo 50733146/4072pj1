60aa8f595db5daa1fd326cc4c72e705c7aca3318
s40723150 <40723150@gm.nfu.edu.tw>
2021-10-04 01:32:13 +0800

    update content

--- 40723150/training data/V1toV2/pong2/pong2.py
+++ 40723150/training data/V1toV2/pong2/pong2.py
@@ -16,7 +16,7 @@ A = 3   # 2, 3 for no-ops
 H = 200 # number of hidden layer neurons
 update_freq = 10
 batch_size = 1000 # every how many episodes to do a param update?
-learning_rate = 1e-4
+learning_rate = 1e-3
 gamma = 0.99 # discount factor for reward
 decay_rate = 0.99 # decay factor for RMSProp leaky sum of grad^2
 
@@ -144,7 +144,7 @@ while True:
     u = np.random.uniform()
     aprob_cum = np.cumsum(aprob)
     a = np.where(u <= aprob_cum)[0][0]
-    action = a+2
+    action = a+1
     #if action==4:action = 0
     #print(action)
     #print(u, a, aprob_cum)
